5eabdfadd384b38e8e1887ca6df2c19bc9700e80
reza salimi <168948763+Rezaslm0@users.noreply.github.com>
2024-08-04 16:50:25 +0330

    fest: I added the files to project

---
+++ exersice/script.js
@@ -0,0 +1,11 @@
+// start to making a calculator
+//get the numbers
+let calc = 0,
+  num1 = 10,
+  num2 = 11;
+//Mathematical operations
+console.log("total:", num1 + num2);
+console.log("minus:", num1 - num2);
+console.log("multiplicsation:", num1 * num2);
+console.log("divison:", num1 / num2);
+console.log("left over:", num1 % num2);
